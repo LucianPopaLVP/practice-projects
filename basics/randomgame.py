764380f960fd227010cee94ad250f7e4bb6b4732
LucianPopaLVP <vasilelucianpopa25@gmail.com>
2021-09-25 16:55:59 +0000

    Create randomgame.py

---
+++ basics/randomgame.py
@@ -0,0 +1,23 @@
+from random import randint
+
+# generate a number 1 - 10
+answer = randint(1, 10)
+
+# imput from user
+# check if imput is a number 1 - 10
+# check if number is right guess. if False
+# ask again
+
+while True:
+    try:
+        guess = int(input('Guess a number 1 - 10:  '))
+        if  0 < guess < 11:
+            if guess == answer:
+                print('You are right!')
+                break
+        else:
+            print('Read carefully! Nees a number 1- 10:  ')
+    except ValueError:
+        print('Please enter a number!!!')
+        continue
+
