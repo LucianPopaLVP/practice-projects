99c81819be890c25308f1a3a4c71566439b4a2dc
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-02 17:14:33 -0200

    .

--- intermediate/translator/translator.py
+++ intermediate/translator/translator.py
@@ -1,5 +1,10 @@
+from translate import Translator
+
+translator = Translator(to_lang='ja')
 try:
     with open('./En-text', mode='r') as my_file:
-        print(my_file.read())
+        text = my_file.read()
+        translation = translator.translate(my_file)
+        print(translation)
 except FileNotFoundError as e:
     print('check your file path silly!!')
