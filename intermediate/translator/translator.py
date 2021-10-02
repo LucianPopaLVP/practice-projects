a3c77a3787496df5595e29b6b2f827939ce89bcf
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-02 17:25:49 -0200

    .

--- intermediate/translator/translator.py
+++ intermediate/translator/translator.py
@@ -2,7 +2,7 @@ from translate import Translator
 
 translator = Translator(to_lang='ja')
 try:
-    with open('./En-text', mode='r') as my_file:
+    with open('./text', mode='r') as my_file:
         text = my_file.read()
         translation = translator.translate(my_file)
         print(translation)
