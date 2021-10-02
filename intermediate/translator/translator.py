bd2c9cca0e7e799a82b6658fdfcfb2069ae23a57
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-02 17:31:40 -0200

    .

--- intermediate/translator/translator.py
+++ intermediate/translator/translator.py
@@ -2,9 +2,9 @@ from translate import Translator
 
 translator = Translator(to_lang='ja')
 try:
-    with open('./text', mode='r') as my_file:
+    with open('test', mode='r') as my_file:
         text = my_file.read()
-        translation = translator.translate(my_file)
+        translation = translator.translate(text)
         print(translation)
-except FileNotFoundError as e:
-    print('check your file path silly!!')
+except FileNotFoundError:
+    print('check path!')
