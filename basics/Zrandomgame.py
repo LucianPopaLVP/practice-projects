b58bade8aa1645c65c42a3de33a7ee412a948515
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-04 17:45:24 -0200

    testing randomgame

---
+++ basics/Zrandomgame.py
@@ -0,0 +1,23 @@
+import random
+from random import randint
+
+def run_guess(guess, answer):
+    if 0 < guess < 11:
+        if guess == answer:
+            print('You are right! You little lucky cookie!')
+            return True
+
+        else:
+            print('Read carefully! I requested a number 1- 10:  ')
+
+answer = randint(1, 10)
+while True:
+    try:
+        guess = int(input('Guess a number 1 - 10:  '))
+        if(run_guess(guess, answer)):
+            break
+
+    except ValueError:
+        print('Please enter a number!!!')
+        continue
+
---
+++ basics/Ztestrandomgame.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestGame(unittest.TestCase):
+    def test_input(self):
+        pass
+
+if __name__ == '__main__':
+    unittest
