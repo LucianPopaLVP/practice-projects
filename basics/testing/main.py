e565100eca5a9454e11d9a7ea22f466f9ba0cfe8
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-04 17:22:29 -0200

    added extra function

--- basics/testing/main.py
+++ basics/testing/main.py
@@ -1,5 +1,8 @@
-def do_stuff(num):
+def do_stuff(num=0):
     try:
-        return int(num) + 5
+        if num:
+            return int(num) + 5
+        else:
+            return 'please, enter a number!'
     except ValueError as err:
         return err
--- basics/testing/test.py
+++ basics/testing/test.py
@@ -12,6 +12,11 @@ class TestMain(unittest.TestCase):
         result = main.do_stuff(test_param)
         self.assertIsInstance(result, ValueError)
 
+    def test_do_stuff3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please, enter a number!')
+
 
 if __name__ == '__main__':
     unittest
