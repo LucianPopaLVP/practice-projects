67cda4498ab0d226addd0ddfcabf5eee69d468bd
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-04 17:14:29 -0200

    testing code

---
+++ basics/testing/test.py
@@ -0,0 +1,17 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+    def test_do_stuff(self):
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 15)
+
+    def test_do_stuff2(self):
+        test_param = 'Lucian'
+        result = main.do_stuff(test_param)
+        self.assertTrue(isinstance(result, ValueError))
+
+
+if __name__ == '__main__':
+    unittest
