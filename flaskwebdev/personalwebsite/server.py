b1cca95dc5b58dd62b3aff55688021f9880c2b3d
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-08 17:49:23 -0200

    coding the website

--- flaskwebdev/personalwebsite/server.py
+++ flaskwebdev/personalwebsite/server.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-print(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Lucian!</p>"
+@app.route('/index.html')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+
+
