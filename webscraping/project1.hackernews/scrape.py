17c3acadbe4bff5f2eb0046ca5924dc9e773c84d
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-08 10:54:24 -0200

    Update scrape.py

--- webscraping/project1.hackernews/scrape.py
+++ webscraping/project1.hackernews/scrape.py
@@ -3,10 +3,20 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://ycombinator.com/news')
+res2 = requests.get('https://ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 # Select CSS Selectors, look for id on web
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
+links2 = soup2.select('.storylink')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
 
 def create_custom_hn(links, subtext):
     hn = []
@@ -18,7 +28,7 @@ def create_custom_hn(links, subtext):
             points = int(vote[0].getText().replace(' points', ''))
             if points > 99:
                 hn.append({'title': title, 'link': href, 'votes': points})
-    return hn
+    return sort_stories_by_votes(hn)
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
 
