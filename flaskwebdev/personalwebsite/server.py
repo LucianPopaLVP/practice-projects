485b2646ed3a3d4ba8ea91d61e6cca600a8a866c
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-10 16:36:40 -0200

    .

--- flaskwebdev/personalwebsite/server.py
+++ flaskwebdev/personalwebsite/server.py
@@ -10,11 +10,18 @@ def home_page():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('/thankyou.html')
     else:
         return 'Something went wrong. Try again!'
