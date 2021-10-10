d5490f3a584687bb2ad44644d1d2f0d8c65963e0
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-10 16:29:09 -0200

    update on website

--- flaskwebdev/personalwebsite/server.py
+++ flaskwebdev/personalwebsite/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -10,6 +10,14 @@ def home_page():
 def html_page(page_name):
     return render_template(page_name)
 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong. Try again!'
 
 
 
