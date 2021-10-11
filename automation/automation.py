73b8819985df5f263b4a5b4f166eb90a25160cbc
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-11 11:01:14 -0200

    Update automation.py

--- automation/automation.py
+++ automation/automation.py
@@ -6,13 +6,14 @@ chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/python/example-code-using-selenium-webdriver-python')
 
 assert 'Selenium Easy' in chrome_browser.title
-#use selectors
+#use CSS selectors
 show_message_button = chrome_browser.find_element_by_class_name('btn-default')
 print(show_message_button.get_attribute('innerHTML'))
 
 assert 'Show Message' in chrome_browser.page_source
 
 user_message = chrome_browser.find_element_by_id('user-message')
+user_button2 = chrome_browser.find_element_by_css_selector('#get-input > .btn')
 user_message.clear()
 user_message.send_keys('I am extra cool!')
 
@@ -20,5 +21,10 @@ show_message_button.click()
 output_message = chrome_browser.find_element_by_id('display')
 
 assert 'I am extra cool!' in output_message.text
+
+chrome_browser.close()
+#or
+#chrome_browser.quit() to close all the sessions
+
 
 
