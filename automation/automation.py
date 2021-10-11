a8b3669eefad173bf302ec0a1c5bcf61d0ae2464
Lucian Popa <88782755+LucianPopaLVP@users.noreply.github.com>
2021-10-11 10:51:08 -0200

    .

--- automation/automation.py
+++ automation/automation.py
@@ -5,4 +5,20 @@ chrome_browser = webdriver.Chrome('./chromedriver')
 chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/python/example-code-using-selenium-webdriver-python')
 
-print('Selenium Easy' in chrome_browser.title)
+assert 'Selenium Easy' in chrome_browser.title
+#use selectors
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+print(show_message_button.get_attribute('innerHTML'))
+
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('I am extra cool!')
+
+show_message_button.click()
+output_message = chrome_browser.find_element_by_id('display')
+
+assert 'I am extra cool!' in output_message.text
+
+
