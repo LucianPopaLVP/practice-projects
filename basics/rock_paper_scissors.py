d6a829e692d32528f8e95b9b7ab5a6beb7135ad3
Lucian Popa <lucianpopa25.lp@gmail.com>
2021-10-27 16:52:16 -0200

    Update rock_paper_scissors.py

--- basics/rock_paper_scissors.py
+++ basics/rock_paper_scissors.py
@@ -26,7 +26,6 @@ scissors = '''
 '''
 import random
 
-rsp = [rock, paper, scissors]
 
 print("Hello, This is Rock, Paper & Scissors Game!!!")
 print("0 - Rock\n1 - Paper\n2 - Scissors")
@@ -34,15 +33,17 @@ print("0 - Rock\n1 - Paper\n2 - Scissors")
 game_images = [rock, paper, scissors]
 
 user_choice = int(input("What do You choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game_images[user_choice])
+if user_choice >= 3 or  user_choice < 0:
+    print("Invalid choice! You lost!")
+else:
+    print(game_images[user_choice])
 
 computer_choice = random.randint(0, 2)
 print(game_images[computer_choice])
 print(f"Computer chose {computer_choice}")
 
-if user_choice >= 3 or  user_choice < 0:
-    print("Invalid choice! You lost!")
-elif user_choice == 0 and computer_choice == 2:
+
+if user_choice == 0 and computer_choice == 2:
     print("You won!")
 elif computer_choice == 0 and user_choice == 2:
     print("You lost!")
@@ -52,3 +53,4 @@ elif user_choice > computer_choice:
     print("You won!")
 elif computer_choice == user_choice:
     print("Draw!")
+
