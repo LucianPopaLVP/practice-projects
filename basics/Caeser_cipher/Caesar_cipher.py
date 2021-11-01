2f8f9218fac3a2f2059869b110a844b2215fa0ee
Lucian Popa <lucianpopa25.lp@gmail.com>
2021-11-01 18:15:42 -0200

    small adjustments

--- basics/Caeser_cipher/Caesar_cipher.py
+++ basics/Caeser_cipher/Caesar_cipher.py
@@ -1,3 +1,6 @@
+from art import logo
+print(logo)
+
 print("Welcome to Caesar Cypher!")
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
