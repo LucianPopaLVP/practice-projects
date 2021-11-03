e4c3e7e1b2276a9471f432fcc7ccfd785fa1907a
Lucian Popa <lucianpopa25.lp@gmail.com>
2021-11-03 18:06:46 -0200

    Update calculator.py

--- basics/calculator/calculator.py
+++ basics/calculator/calculator.py
@@ -35,6 +35,14 @@ for symbol in operations:
 operation_symbol = input("Pick an operation from the line above: ")
 calculation_function = operations[operation_symbol]
 num2 = int(input("What's the second number?: "))
-answer = calculation_function(num1, num2)
+first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What's the third number?: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(calculation_function(num1, num2), num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+
