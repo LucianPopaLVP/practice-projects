b6d15b6b78e3035b6c6c67608451268628bf8b12
Lucian Popa <lucianpopa25.lp@gmail.com>
2021-11-03 18:24:24 -0200

    Update calculator.py

--- basics/calculator/calculator.py
+++ basics/calculator/calculator.py
@@ -1,23 +1,23 @@
+import os
 from art import logo
-print(logo)
 
-print("Welcome!")
+clear = lambda: os.system('cls')
 
-#Add
 def add(n1, n2):
     return n1 + n2
 
-#Subtract
+
 def subtract(n1, n2):
     return n1 - n2
 
-#Multiply
+
 def multiply(n1, n2):
     return n1 * n2
 
-#Divide
+
 def divide(n1, n2):
     return n1 / n2
+
 
 operations = {
     "+": add,
@@ -26,23 +26,28 @@ operations = {
     "/": divide
 }
 
-function = operations["*"]
-function(2, 3)
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-calculation_function = operations[operation_symbol]
-num2 = int(input("What's the second number?: "))
-first_answer = calculation_function(num1, num2)
+def calculator():
+    print(logo)
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the third number?: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(calculation_function(num1, num2), num3)
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            clear()
+            calculator()
 
+
+calculator()
