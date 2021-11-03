b55943d264c56e0653ba91125feb1a72001d72ff
Lucian Popa <lucianpopa25.lp@gmail.com>
2021-11-03 17:59:55 -0200

    update

--- basics/calculator/calculator.py
+++ basics/calculator/calculator.py
@@ -3,3 +3,38 @@ print(logo)
 
 print("Welcome!")
 
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+function = operations["*"]
+function(2, 3)
+
+num1 = int(input("What's the first number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+calculation_function = operations[operation_symbol]
+num2 = int(input("What's the second number?: "))
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
