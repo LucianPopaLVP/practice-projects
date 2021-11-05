0d7125d37888dc4e07147ae82cdd0310382217cd
Lucian Popa <lucianpopa25.lp@gmail.com>
2021-11-05 17:47:09 -0200

    Update main.py

--- basics/higher_lower game/main.py
+++ basics/higher_lower game/main.py
@@ -5,6 +5,7 @@ from game_data import data
 
 clear = lambda: os.system('cls')
 print(logo)
+score = 0
 
 def format_data(account):
     """Format the account data into printable format."""
@@ -15,7 +16,11 @@ def format_data(account):
 
 def check_answer(guess, a_followers, b_followers):
     """Take the user guess and follower counts and returns if they got it right"""
-    
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+        
 account_a = random.choice(data)
 account_b = random.choice(data)
 if account_a == account_b:
@@ -27,6 +32,12 @@ print(f"Compare B: {format_data(account_b)}.")
 
 guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-a_follower_account = account_a["follower_count"]
-b_follower_account = account_b["follower_count"]
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
+if is_correct:
+    score += 1
+    print(f"You are right! Current score: {score}.")
+else:
+    print(f"Sorry, that's wrong! Final score: {score}")
