0321dc95a9a30e9a2dc1a6dfc70d735408602f5a
Lucian Popa <lucianpopa25.lp@gmail.com>
2021-11-05 17:31:10 -0200

    .

---
+++ basics/higher_lower game/main.py
@@ -0,0 +1,22 @@
+import os
+import random
+from art import logo
+from game_data import data
+
+clear = lambda: os.system('cls')
+print(logo)
+
+def format_data(account):
+    """Format the account data into printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+    account_b = random.choice(data)
+
+print(f"Compare A: {format_data(account_a)}.")
+print(f"Compare A: {format_data(account_b)}.")
