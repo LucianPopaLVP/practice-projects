5f067a4ee61391e2d020633ff7edae60112e7a5a
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-08 17:50:16 -0200

    .

--- intermediate/coffee machine/coffee_machine_in_OOP.py
+++ intermediate/coffee machine/coffee_machine_in_OOP.py
@@ -3,3 +3,7 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+coffee_maker.report()
+money_machine.report()
