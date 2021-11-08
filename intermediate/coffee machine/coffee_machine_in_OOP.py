1daab41ef9ad364b689e0ecd667edecc29cbbf09
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-08 17:54:15 -0200

    Update coffee_machine_in_OOP.py

--- intermediate/coffee machine/coffee_machine_in_OOP.py
+++ intermediate/coffee machine/coffee_machine_in_OOP.py
@@ -4,6 +4,15 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
 
 coffee_maker.report()
 money_machine.report()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    
