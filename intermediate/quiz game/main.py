234b12b637c075fbcd4dec27d8e95bbe65a41d5d
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-09 18:02:05 -0200

    updates

--- intermediate/quiz game/main.py
+++ intermediate/quiz game/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -8,4 +9,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-QuisBrain
+quiz = QuizBrain(question_bank)
+quiz.next_question()
