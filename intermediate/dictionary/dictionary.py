d09018c1c3b60764cbb0b2aae499160305ea5636
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-14 07:55:58 -0200

    Update dictionary.py

--- intermediate/dictionary/dictionary.py
+++ intermediate/dictionary/dictionary.py
@@ -12,7 +12,7 @@ def translate(word):
     elif word.upper() in data:
         return data[word.upper()]
     # Get the close matches
-    elif:
+    elif len(get_close_matches())
     else:
         print("Word not found!")
 
