fcecf4e67e6e6f7f3b2fd87a616403599edd9ef5
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-14 07:32:24 -0200

    Update dictionary.py

--- intermediate/dictionary/dictionary.py
+++ intermediate/dictionary/dictionary.py
@@ -2,5 +2,12 @@ import json
 
 data = json.load(open("data.json"))
 
+def translate(word):
+    if word in data:
+        return data[word]
+    else:
+        print("Word not found!")
 
-
+word = input("Please enter the word you want to search: ")
+output = translate(word)
+print(output)
