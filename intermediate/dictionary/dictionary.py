48e03bc1c326d93e6c58f3b0256f4753c5796f5a
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-14 07:45:03 -0200

    Update dictionary.py

--- intermediate/dictionary/dictionary.py
+++ intermediate/dictionary/dictionary.py
@@ -3,8 +3,13 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
+    word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     else:
         print("Word not found!")
 
