6674f5152a38c58871cb324f66fc94e8761db720
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-14 07:13:57 -0200

    Create dice_simulator.py

---
+++ basics/dice_simulator.py
@@ -0,0 +1,45 @@
+import random   
+
+number = random.randint(1, 6)
+
+if number == 1:
+    print("-----------")
+    print("|         |")
+    print("|    0    |")
+    print("|         |")
+    print("-----------")
+
+if number == 2:
+    print("-----------")
+    print("|         |")
+    print("|    0    |")
+    print("|         |")
+    print("-----------")
+
+if number == 3:
+    print("-----------")
+    print("|         |")
+    print("|    0    |")
+    print("|         |")
+    print("-----------")
+
+if number == 4:
+    print("-----------")
+    print("|         |")
+    print("|    0    |")
+    print("|         |")
+    print("-----------")
+
+if number == 5:
+    print("-----------")
+    print("|         |")
+    print("|    0    |")
+    print("|         |")
+    print("-----------")
+
+if number == 6:
+    print("-----------")
+    print("|         |")
+    print("|    0    |")
+    print("|         |")
+    print("-----------")
