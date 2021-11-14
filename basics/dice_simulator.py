cf755dd5d5e5a0acfa1d7a464830a1bebdf187fe
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-14 07:15:10 -0200

    Update dice_simulator.py

--- basics/dice_simulator.py
+++ basics/dice_simulator.py
@@ -12,34 +12,34 @@ if number == 1:
 if number == 2:
     print("-----------")
     print("|         |")
-    print("|    0    |")
+    print("|  0   0  |")
     print("|         |")
     print("-----------")
 
 if number == 3:
     print("-----------")
-    print("|         |")
     print("|    0    |")
-    print("|         |")
+    print("|    0    |")
+    print("|    0    |")
     print("-----------")
 
 if number == 4:
     print("-----------")
+    print("|  0   0  |")
     print("|         |")
-    print("|    0    |")
-    print("|         |")
+    print("|  0   0  |")
     print("-----------")
 
 if number == 5:
     print("-----------")
-    print("|         |")
+    print("| 0     0 |")
     print("|    0    |")
-    print("|         |")
+    print("| 0     0 |")
     print("-----------")
 
 if number == 6:
     print("-----------")
-    print("|         |")
-    print("|    0    |")
-    print("|         |")
+    print("|  0    0 |")
+    print("|  0    0 |")
+    print("|  0    0 |")
     print("-----------")
