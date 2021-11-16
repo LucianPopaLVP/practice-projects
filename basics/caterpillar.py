42998b8c71dce8b706b0f600de059c2bf37b3d01
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2021-11-16 17:31:53 -0200

    Update caterpillar.py

--- basics/caterpillar.py
+++ basics/caterpillar.py
@@ -1,6 +1,6 @@
 import turtle as t
 import random as rd
-import time as time
+import time as ti
 
 t.bgcolor('yellow')
 
@@ -8,16 +8,44 @@ caterpillar = t.Turtle()
 caterpillar.shape('square')
 caterpillar.speed(0)
 caterpillar.penup()
+caterpillar.hideturtle()
 
 leaf = t.Turtle()
 leaf_shape = ((0,0),(14,2),(18,6),(20,20),(6,18),(2,14))
-t.register_shape('leaf')
+t.register_shape('leaf', leaf_shape)
+leaf.shape('leaf')
+leaf.color('green')
+leaf.penup()
+leaf.speed()
+leaf.hideturtle()
 
 text_turtle = False
 text_turtle = t.Turtle()
 text_turtle.write('Press space to start', align='center', font=('Arial',10,'bold'))
+text_turtle.hideturtle()
 
 score_turtle = t.Turtle()
 score_turtle.hideturtle()
 score_turtle.speed(0)
+
+def outside_window():
+    left_wall = -t.window_width() / 2
+    right_wall = t.window_width() / 2
+    top_wall =  t.window_height() / 2
+    bottom_wall = -t.window_height() / 2
+    (x, y) = caterpillar.pos()
+    outside = x < left_wall or x > right_wall or y >  top_wall or y < bottom_wall
+    return outside
+
+def place_leaf():
+    leaf.hideturtle()
+    leaf.setx(rd.randint(-200, 200))
+    leaf.sety(rd.randint(-200, 200))
+    leaf.showturtle()
+    
+def game_over():
+
+def display_score():
+
+ti.sleep(3)
 
