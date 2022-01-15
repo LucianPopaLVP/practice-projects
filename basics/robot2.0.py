8bd13979c5a83b0338c49a7d1b145a58dc39d4aa
LucianPopaLVP <lucianpopa25.lp@gmail.com>
2022-01-15 13:24:04 +0200

    Update robot2.0.py

--- basics/robot2.0.py
+++ basics/robot2.0.py
@@ -6,3 +6,10 @@ def rectangle(hor,ver,col):
     t.pensize(1)
     t.color(col)
     t.begin_fill()
+    for counter in range(1, 3):
+        t.forward(hor)
+        t.right(90)
+        t.forward(ver)
+        t.right(90)
+    t.end_fill()
+    t.penup()
